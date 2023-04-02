212bb98a70f25ef18582c1c97e6849b41883b93f
EC2 Default User <ec2-user@ip-172-31-30-185.ap-northeast-1.compute.internal>
2023-04-02 16:28:14 +0000

    favorite

--- app/models/post_comment.rb
+++ app/models/post_comment.rb
@@ -1,4 +1,5 @@
 class PostComment < ApplicationRecord
     belongs_to :user
     belongs_to :book
+    validates :comment, length: { minimum: 2, maximum: 200 }, uniqueness: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
   end
 
   resources :books, only: [:index,:show,:edit,:create,:destroy,:update] do
+    resource :favorites, only: [:create, :destroy]
     resources :post_comments, only: [:create, :destroy]
   end
   resources :users, only: [:index,:show,:edit,:update]
